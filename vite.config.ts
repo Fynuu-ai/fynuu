51232059781a8e7584e6300984b924af6ba9b06f
nikhilkeetha <nikhilkeethanikhil@gmail.com>
2025-10-17 22:38:00 +0530

    change mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/fynuu/',
+  base: mode === "development" ? "/" : "/fynuu/",
   server: {
     host: "::",
     port: 8080,
